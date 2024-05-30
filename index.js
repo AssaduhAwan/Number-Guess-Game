fc9e0d1eec17eb1d6a89eb76c435bc14a40a4481
AsssaduhAwan <astudentsgithub@gmail.com>
2024-05-30 15:54:29 +0500

    Add new project

---
+++ index.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+let score = 0;
+async function startloop() {
+    let again;
+    do {
+        await guessNumber();
+        again = await inquirer.prompt([
+            {
+                type: "list",
+                name: "restart",
+                choices: ["yes", "NO"],
+                message: chalk.green("Do you want to contiune :")
+            }
+        ]);
+    } while (again.restart === "yes");
+}
+;
+startloop();
+async function guessNumber() {
+    let guessNum = Math.floor(Math.random() * 10);
+    let tip;
+    if (guessNum % 2 == 0) {
+        tip = (chalk.blue(`Tip: Number is even.`));
+    }
+    else {
+        tip = (chalk.green(`Tip: Number is odd.`));
+    }
+    const answer = await inquirer.prompt([
+        {
+            type: "number",
+            name: "userguess",
+            message: chalk.yellow(`Guess a number between 1 to 10 (${tip}) `)
+        }
+    ]);
+    console.log(chalk.blue(`Your guess ?.${answer.userguess} and system generates ${guessNum}`));
+    if (answer.userguess === guessNum) {
+        score++;
+        console.log(chalk.cyanBright(`congralulations your answer is correct.Your score is:${score}`));
+    }
+    else {
+        console.log(chalk.red(`Wrong guess ? Your score is  ${score}.butter luck nexttime.`));
+    }
+}
+;
